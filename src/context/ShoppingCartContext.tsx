9be0032954b42ef321b79ccb6ae63a0d4a86749f
Jamal-Deallie <66758645+Jamal-Deallie@users.noreply.github.com>
2022-08-24 00:01:45 -0500

    added cart functionality to cart context

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,7 +1,23 @@
-import { createContext, useContext } from 'react';
+import { useState, createContext, useContext } from 'react';
 
 type ShoppingCartProviderProps = {
   children: React.ReactNode;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
+  openCart: () => void;
+  closeCart: () => void;
 };
 
 const ShoppingCartContext = createContext({});
@@ -11,8 +27,69 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  function getItemQuantity(id: number) {
+    return cartItems.find(item => item.id === id)?.quantity || 0;
+  }
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
+  
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+  function increaseCartQuantity(id: number) {
+    setCartItems(currItems => {
+      if (currItems.find(item => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map(item => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function decreaseCartQuantity(id: number) {
+    setCartItems(currItems => {
+      //if quantity is equal to one remove it with filter by return items that don't match the params id
+      if (currItems.find(item => item.id === id)?.quantity === 1) {
+        return currItems.filter(item => item.id !== id);
+      } else {
+        return currItems.map(item => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function removeFromCart(id: number) {
+    //remove item with filter by return items that don't match the params id
+    setCartItems(currItems => {
+      return currItems.filter(item => item.id !== id);
+    });
+  }
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+        openCart,
+        closeCart,
+        cartItems,
+        cartQuantity,
+      }}>
       {children}
     </ShoppingCartContext.Provider>
   );
